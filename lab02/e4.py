39c0f1d846b414e5e089bbe719238fdde0bd9a4a
OrfeasANikolaou <orfeasnikolaou.dev@gmail.com>
2024-03-21 19:21:12 +0200

    fixed

--- lab02/e4.py
+++ lab02/e4.py
@@ -35,22 +35,22 @@ def f4(lst):
 class TestComprehensions(unittest.TestCase):
     def test1(self):
         a_list = [56, 37, 771, 90, 16, 11]
-        b_list = f1(a_list)
+        b_list = [len(str(obj)) for obj in a_list]
         self.assertEqual(b_list, [2,2,3,2,2,2])         
 
     def test2(self):
         a_list = [56, 37, 771, 90, 16, 11]
-        b_list = f2(a_list)
+        b_list = [int(str(obj)[::-1]) for obj in a_list] 
         self.assertEqual(b_list, [65,73,177,9,61,11])         
 
     def test3(self):
         a_list = [56, 37, 771, 90, 16, 11]
-        b_list = f3(a_list) 
+        b_list = [obj for obj in a_list if obj > sum(a_list)/len(a_list)] 
         self.assertEqual(b_list, [771])         
 
     def test4(self):
         a_list = [56, 37, 771, 90, 16, 11]
-        b_list = f4(a_list) 
+        b_list = [(obj, obj%2==0) for obj in a_list]
         self.assertEqual(b_list, [(56,True), (37,False), (771,False), (90, True), (16, True), (11, False)]) 
 
 if __name__ == "__main__":
